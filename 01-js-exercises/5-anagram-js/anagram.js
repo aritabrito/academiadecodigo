35e319bcd043da66edc8b375b745f2cde380c2c9
Rita Brito <mailtoritabrito@gmail.com>
2018-01-22 09:32:03 +0000

    Update anagram.js

--- 01-js-exercises/5-anagram-js/anagram.js
+++ 01-js-exercises/5-anagram-js/anagram.js
@@ -5,15 +5,16 @@ var word1 = "ana";
 var word2 = "naa"
 
 function isAnagram(word1, word2){
-
-  var word1Sorted = word1.split("").sort().join("");
-  var word2Sorted = word2.split("").sort().join("");
-
+  
+  var word1Sorted = word1.toLowerCase().split("").sort().join("");
+  var word2Sorted = word2.toLowerCase().split("").sort().join("");
+  
   if (word1Sorted !== word2Sorted) {
     return false;
   }
   return true;
-
+ 
 }
 
 console.log(word1 + " and "+ word2 + " are anagrams: " + isAnagram(word1, word2) +".");
+
