1a74eed6e87033cab3ffc583279e6beb2717240f
Rita Brito <mailtoritabrito@gmail.com>
2018-01-22 09:33:21 +0000

    Update Anagram.java

--- 02-java-exercises/5-anagram-java/src/Anagram.java
+++ 02-java-exercises/5-anagram-java/src/Anagram.java
@@ -5,7 +5,7 @@ public class Anagram {
 
     public static void main(String[] args) {
 
-        String wordOne = "rita";
+        String wordOne = "Rita";
         String wordTwo = "aitr";
 
         System.out.println(wordOne + " and " + wordTwo + " are anagrams: " + isAnagram(wordOne, wordTwo));
@@ -14,22 +14,26 @@ public class Anagram {
     /* ANAGRAM */
 
     public static boolean isAnagram(String strOne, String strTwo) {
-        strOne = sortString(strOne);
-        strTwo = sortString(strTwo);
+      strOne = strOne.toLowerCase();
+      strTwo = strTwo.toLowerCase();
+      
+      strOne = sortString(strOne);
+      strTwo = sortString(strTwo);
+        
 
-        if (strOne.length() != strTwo.length()) {
-            return false;
-        }
+      if (strOne.length() != strTwo.length()) {
+          return false;
+      }
 
-        if (!strOne.contains(strTwo)) {
-            return false;
-        }
-        return true;
+      if (!strOne.contains(strTwo)) {
+          return false;
+      }
+      return true;
     }
 
     private static String sortString(String strToSort) {
-        char[] charArray = strToSort.toCharArray();
-        Arrays.sort(charArray);
-        return new String (charArray);
+      char[] charArray = strToSort.toCharArray();
+      Arrays.sort(charArray);
+      return new String (charArray);
     }
 }
